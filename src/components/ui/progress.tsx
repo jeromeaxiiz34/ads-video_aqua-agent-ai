46f42e635c12e1e446e35649becd8fc8faeb0ce4
jeromeaxiiz34 <117950995+jeromeaxiiz34@users.noreply.github.com>
2025-10-19 17:40:06 +0200

    Update progress.tsx

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -13,9 +13,13 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
+  className="h-full w-full flex-1 transition-all"
+  style={{
+    transform: `translateX(-${100 - (value || 0)}%)`,
+    backgroundColor: "#f47014", // 🔥 ta nouvelle couleur ici
+  }}
+/>
+
   </ProgressPrimitive.Root>
 ));
 Progress.displayName = ProgressPrimitive.Root.displayName;
